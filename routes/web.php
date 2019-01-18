e5c48bfd4b9af056a534416e83d1f2a9a06a652e
why8282 <garywong8282@gmail.com>
2019-01-18 17:16:40 +0800

    Post Weibo

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,13 @@ Route::get('/', function () {
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-Route::get('signup', 'UsersController@create')->name('signup');
-Route::resource('users', 'UsersController');
+
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 
@@ -31,3 +33,5 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
