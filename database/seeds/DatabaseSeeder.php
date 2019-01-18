7ee98c084aca4b75cd8c9fb5d063de7e78cf5f18
why8282 <garywong8282@gmail.com>
2019-01-18 17:03:30 +0800

    User weibo list

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // Model::unguard();
+        Model::unguard();
 
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
 
         Model::reguard();
     }
